af270ade072a31b5c29d2efeab54aea4e48eabac
denghb <i@denghb.com>
2019-06-17 22:07:05 +0800

    update

--- src/main/java/com/denghb/App.java
+++ src/main/java/com/denghb/App.java
@@ -6,29 +6,29 @@ import java.util.*;
  * Hello world!
  */
 public class App {
-    private static List<Commodity> list = new ArrayList<Commodity>();
-
-    static {
-
-        list.add(new Commodity(1, "光明牛奶", 241, 5, 3));
-        list.add(new Commodity(2, "可口可乐", 356, 5, 3));
-        list.add(new Commodity(3, "农夫山泉", 513, 5, 3));
-        list.add(new Commodity(4, "脉动饮料", 756, 5, 3));
-//        list.add(new Commodity(5, "红牛饮料", 485, 5, 20));
-    }
 
     public static void main(String[] args) {
+        List<Commodity> input = new ArrayList<Commodity>();
+        input.add(new Commodity(1, "光明牛奶", 241, 5, 2));
+        input.add(new Commodity(2, "可口可乐", 356, 5, 3));
+        input.add(new Commodity(3, "农夫山泉", 513, 5, 4));
+        input.add(new Commodity(4, "脉动饮料", 756, 5, 5));
+//        input.add(new Commodity(5, "红牛饮料", 485, 5, 20));
+
+        Set<String> union = new HashSet<>();
 
         long start = System.currentTimeMillis();
         // 全组合 weight , 商品列表
         List<CommodityGroup> output = new ArrayList<>();
-        for (int i = 0; i < list.size(); i++) {
-            Commodity c = list.get(i);
+        for (int i = 0; i < input.size(); i++) {
+            Commodity c = input.get(i);
             for (int n = 1; n <= c.getNum(); n++) {
                 int w = c.getWeight() * n;
                 List<Commodity> cList = Arrays.asList(new Commodity(c, n));
                 output.add(new CommodityGroup(w, cList));
-                doGroup(i, cList, output);
+                union.add(c.getId() + ":" + n + ";");
+
+                doGroup(i, input, union, cList, output);
             }
 
         }
@@ -59,12 +59,12 @@ public class App {
 
     }
 
-    private static void doGroup(int i, List<Commodity> cList, List<CommodityGroup> output) {
+    private static void doGroup(int i, List<Commodity> input, Set<String> union, List<Commodity> cList, List<CommodityGroup> output) {
 
-        for (int next = 1; next < list.size(); next++) {
+        for (int next = 1; next < input.size(); next++) {
             List<Commodity> c2List = new ArrayList<Commodity>(cList);
-            for (int j = i + next; j < list.size(); j++) {
-                Commodity c = list.get(j);
+            for (int j = i + next; j < input.size(); j++) {
+                Commodity c = input.get(j);
                 for (int n = 1; n <= c.getNum(); n++) {
 
                     // 存在的改变数量
@@ -82,15 +82,20 @@ public class App {
 
                     int weight = 0;
                     List<Commodity> c3List = new ArrayList<Commodity>();
+
+                    String key = "";
                     for (Commodity c2 : c2List) {
                         weight += c2.getNum() * c2.getWeight();
                         c3List.add(new Commodity(c2, c2.getNum()));// 防止对象引用
+                        key += c2.getId() + ":" + c2.getNum() + ";";
                     }
 
-                    output.add(new CommodityGroup(weight, c3List));
-
-                    if (c.getNum() > 1 && n < c.getNum()) {
-                        doGroup(j, c2List, output);
+                    if (!union.contains(key)) {
+                        union.add(key);
+                        output.add(new CommodityGroup(weight, c3List));
+                    }
+                    if (c.getNum() > 1) {
+                        doGroup(j, input, union, c2List, output);
                     }
                 }
             }
